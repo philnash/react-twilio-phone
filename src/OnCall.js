5aedd63872acaf13bc7c6698dae28337281f953b
Phil Nash <philnash@gmail.com>
2020-03-23 11:36:35 +1100

    Styled mute button

--- src/OnCall.js
+++ src/OnCall.js
@@ -1,22 +1,27 @@
 import React, { useState } from "react";
 import KeypadButton from "./KeypadButton";
+import "./OnCall.css";
 
 const OnCall = ({ handleHangup, connection }) => {
   const [muted, setMuted] = useState(false);
 
   const handleMute = () => {
-    connection.mute(!muted);
+    // connection.mute(!muted);
     setMuted(!muted);
   };
 
   return (
     <div className="call">
-      <KeypadButton handleClick={handleMute}>
-        {muted ? "Unmute" : "Mute"}
-      </KeypadButton>
-      <KeypadButton handleClick={handleHangup} color="red">
-        Hang up
-      </KeypadButton>
+      <div className="call-options">
+        <KeypadButton handleClick={handleMute} color={muted ? "red" : ""}>
+          {muted ? "Unmute" : "Mute"}
+        </KeypadButton>
+      </div>
+      <div className="hang-up">
+        <KeypadButton handleClick={handleHangup} color="red">
+          Hang up
+        </KeypadButton>
+      </div>
     </div>
   );
 };
